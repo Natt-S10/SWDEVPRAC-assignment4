a61476e187d7913648afcd1ef29f7cc4c6488ef8
Natthaphon Suphansri <natthaphon.s2018@gmail.com>
2023-02-02 01:58:29 +0700

    Finishing

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const hospitals = require('./routes/hospitals');
+
+const app = express();
+
+// Mount routers
+app.use('/api/v1/hospitals', hospitals);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
