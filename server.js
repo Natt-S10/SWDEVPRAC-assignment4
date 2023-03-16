b150d1efc22b69fc4eb1158b9f08cc6abfcce282
Natt-s <Natthaphon.s2018@gmail.com>
2023-03-16 18:28:50 +0700

    Revert "Assignment 7"
    This reverts commit 8b7e7290ae620452915e47dbf5b1d392e5d95af6.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ connectDB();
 
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
 
 const app = express();
 // Body parser
@@ -21,7 +20,7 @@ app.use(cookieParser());
 // Mount routers
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
